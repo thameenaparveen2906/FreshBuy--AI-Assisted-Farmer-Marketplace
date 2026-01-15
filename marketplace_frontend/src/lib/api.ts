b4aa5cfc1611a64d1d7b04686963ba35e901b32e
MACBOOK AIR <macbookair@MacBook-Air.local>
2026-01-15 16:30:19 +0530

    Configure frontend for production API

--- marketplace_frontend/src/lib/api.ts
+++ marketplace_frontend/src/lib/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const baseURL = import.meta.env.VITE_API_BASE_URL;
 
-export const baseURL = "http://127.0.0.1:8000";
+// export const baseURL = "http://127.0.0.1:8000";
 
 export const api = axios.create({
   baseURL: baseURL
