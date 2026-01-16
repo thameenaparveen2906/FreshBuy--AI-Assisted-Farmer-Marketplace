5b2197728db322185763922681f3cdb2fe5739c9
Thameena Parveen <thameenaparveenofficial@gmail.com>
2026-01-16 16:54:28 +0530

    Refactor BASE_DIR to use Path

--- marketplace_backend/marketplace_backend/settings.py
+++ marketplace_backend/marketplace_backend/settings.py
@@ -21,8 +21,7 @@ import cloudinary.api
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
