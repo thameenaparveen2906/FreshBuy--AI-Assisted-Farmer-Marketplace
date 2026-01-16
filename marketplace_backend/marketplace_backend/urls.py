d552771c0201a7f833bc1ea548f3fe670d0bcbba
Thameena Parveen <thameenaparveenofficial@gmail.com>
2026-01-16 16:21:38 +0530

    Simplify MEDIA_URL static handling in urls.py

--- marketplace_backend/marketplace_backend/urls.py
+++ marketplace_backend/marketplace_backend/urls.py
@@ -27,7 +27,4 @@ urlpatterns = [
     path('token_refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
