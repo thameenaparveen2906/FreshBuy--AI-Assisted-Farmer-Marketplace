73b3e217e68b2b631a1eb537a03d0e390236456b
Thameena Parveen <thameenaparveenofficial@gmail.com>
2026-01-16 14:25:09 +0530

    Fix MEDIA_URL and MEDIA_ROOT settings

--- marketplace_backend/marketplace_backend/settings.py
+++ marketplace_backend/marketplace_backend/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
-MEDIA_URL = 'img/'
-MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'core.CustomUser'
 
