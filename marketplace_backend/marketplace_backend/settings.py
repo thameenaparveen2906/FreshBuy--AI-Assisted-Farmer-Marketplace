84b248afb720bcf2876103bb8cb2d80e70d3b23d
Thameena Parveen <thameenaparveenofficial@gmail.com>
2026-01-16 16:23:26 +0530

    Refactor settings.py for BASE_DIR and media paths

--- marketplace_backend/marketplace_backend/settings.py
+++ marketplace_backend/marketplace_backend/settings.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -134,8 +134,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
-MEDIA_URL = 'img/'
-MEDIA_ROOT = BASE_DIR/'media'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 AUTH_USER_MODEL = 'core.CustomUser'
 
